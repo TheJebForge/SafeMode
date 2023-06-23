bc5b30e9073584db18885c8fc330434ef37f0707
TheJebForge <jeb@thejebforge.com>
2023-06-23 21:21:04 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+﻿# Safe Mode
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that adds a configuration toggle to disable all impulse Logix from functioning, so you can use your inspector without Logix being able to do anything. 
+
+Intended to be used as a way to be able to clean your avatar from any viruses that you could had saved from a public session (looking at you Tailroot -.-)
+### Enabling this mod in public worlds might break various world systems like avatar culling, and anything that executes impulses from you
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [SafeMode.dll](https://github.com/TheJebForge/SafeMode/releases/latest/download/SafeMode.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ SafeMode/SafeMode.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BaseX;
+using FrooxEngine.LogiX;
+
+namespace SafeMode
+{
+    public class SafeMode : NeosMod
+    {
+        public override string Name => "SafeMode";
+        public override string Author => "TheJebForge";
+        public override string Version => "1.0.0";
+
+        [AutoRegisterConfigKey]
+        readonly ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled","Safe Mode enabled", () => false);
+
+        private ModConfiguration config;
+        private static SafeMode modInstance;
+
+        bool IsModEnabled() => config.GetValue(ENABLED);
+        
+        public override void OnEngineInit()
+        {
+            modInstance = this;
+            config = GetConfiguration();
+            
+            Harmony harmony = new Harmony($"net.{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Impulse), nameof(Impulse.Trigger))]
+        class ImpulseTrigger
+        {
+            public static bool Prefix()
+            {
+                return !modInstance.IsModEnabled();
+            }
+        }
+    }
+}
